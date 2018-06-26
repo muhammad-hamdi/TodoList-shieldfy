80a6a412f7e2e85d72c3d5e3bf1d2df46131d07f
Muhammad Hamdi <prime.energy7@gmail.com>
2018-06-26 13:50:07 +0200

    finished feature test of the todos

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('todos', 'TodoController@index')->name('api.todos.index');
+Route::get('todos', 'Api\TodoController@index')->name('api.todos.index');
+Route::post('todos', 'Api\TodoController@store')->name('api.todos.store');
+Route::delete('todos/{todo}', 'Api\TodoController@destroy')->name('api.todos.destroy');
--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Todo;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -20,9 +21,47 @@ class TodoTest extends TestCase
 
         $response->assertJsonStructure([
             'data' => [
+                [
                 'id',
                 'content'
-            ]
+                ]
+            ],
         ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_store_new_todos()
+    {
+        $data = [
+            'content' => 'Do more tests!'
+        ];
+
+        $response = $this->postJson(route('api.todos.store'), $data);
+
+        $response->assertSuccessful();
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'content'
+            ],
+            'message'
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_delete_todos()
+    {
+        $todo = factory(Todo::class)->create();
+
+        $response = $this->deleteJson(route('api.todos.destroy', $todo));
+
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $this->assertDatabaseMissing('todos', ['id' => $todo->id, 'content' => $todo->content]);
     }
 }
